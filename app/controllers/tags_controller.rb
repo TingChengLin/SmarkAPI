48cac64ccf67743202136aeae46c4b9f395a2429
TingChengLin <victor.lin@storysensecomputing.com>
2013-06-02 11:39:18 +0800

    edit tag search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   def search
     render :json => {
                       :code => 200,
-                      :tags => Tag.search(params[:query])
+                      :tags => Tag.instance_search(params[:query])
                     }
   end
 end
