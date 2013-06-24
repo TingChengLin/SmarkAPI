a661e73c8814b2175bd9e969e01ec3b2d2d17bab
TingChengLin <victor.lin@storysensecomputing.com>
2013-06-24 23:34:40 +0800

    add callback to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,25 +1,26 @@
 class TagsController < ApplicationController
 
   def index
-    render :json => { :tags => Tag.select(:name).all }
+    render :json => { :tags => Tag.select(:name).all },
+             :callback => params[:callback]
   end
 
   def show
     tag = Tag.find(params[:id])
     bookmarks = tag.bookmarks
-    render :json => (bookmarks.map &lambda { |b| b.profile })
+    render :json => (bookmarks.map &lambda { |b| b.profile }),
+             :callback => params[:callback]
   end
 
   def search
-    render :json => [ Tag.instance_search(params[:query]) ]
+    render :json => [ Tag.instance_search(params[:query]) ],
+             :callback => params[:callback]
   end
 
   def top
     tags = Tag.page(params[:page]).per(params[:per]).map &lambda { |t| t.profile }
-    render :json => {
-      :code => 200,
-      :tags => tags
-    }
+    render :json => { :code => 200, :tags => tags },
+             :callback => params[:callback]
   end
 
 end
