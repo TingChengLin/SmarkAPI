e924c808f3ff943080ea27dc98769906226cfc16
Ubuntu <ubuntu@ip-172-31-7-251.ap-northeast-1.compute.internal>
2013-07-05 17:33:34 +0000

    pushed in server

--- config/initializers/evernote.rb
+++ config/initializers/evernote.rb
@@ -44,6 +44,7 @@ end
 
 def create_notes_of_a_tag(tag)
   bookmarks = tag.bookmarks.where("created_at > ?", Time.now - 1.month)
+  return if bookmarks.empty?
 
   content = "<?xml version='1.0' encoding='UTF-8'?>" +
             "<!DOCTYPE en-note SYSTEM 'http://xml.evernote.com/pub/enml2.dtd'>" +
