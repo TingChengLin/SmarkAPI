d85c6369f21cd5c6bc10cfcb721517ce7caabaad
TingChengLin <victor.lin@storysensecomputing.com>
2014-02-28 10:25:58 +0800

    add finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
       {:title => "資種駭客鬆的第一堂課",
        :speaker => "駭客",
        :info => "1. 跟資種有關",
-       :dealine => DateTime.new(2014,2,28,21,0,0,'+8')}
+       :dealine => DateTime.new(2014,2,28,21,0,0,'+8'),
+       :finished => 8}
     ]
   end
 end
