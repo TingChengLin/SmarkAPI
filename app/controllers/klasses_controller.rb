d4d46e2220620ce3f4d2022ea1efdd1e31371ee4
TingChengLin <victor.lin@storysensecomputing.com>
2014-02-28 11:21:12 +0800

    bug fix

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,6 +1,6 @@
 class KlassesController < ApplicationController
   def status
-    klasses = Klass.page(param[:page]).per(param[:per])
+    klasses = Klass.page(params[:page]).per(params[:per])
     k_profiles = klasses.map {|k| k.profile}
     render :json => k_profiles
   end
